6993c494c86f2f9396df0296a6bab91e696495c6
Arundhathi2282 <arundhathikuricheti@gmail.com>
2021-05-26 12:43:23 +0530

    Add : Calculating 3dr expression

--- arithmaticComputation.sh
+++ arithmaticComputation.sh
@@ -4,5 +4,7 @@ read -p "Enter 2nd input : " b
 read -p "Enter 3rd input : " c
 w=`awk 'BEGIN{print '$a' + '$b' * '$c' }'`
 x=`awk 'BEGIN{print '$a' * '$b' + '$c' }'`
+y=`awk 'BEGIN{print '$c' + '$a' / '$b' }'`
 echo "First expression output : $w"
 echo "Second expression output : $x"
+echo "Third expression output : " $y
