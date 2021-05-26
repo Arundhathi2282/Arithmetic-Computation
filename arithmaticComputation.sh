ff344297cfdb150eca4138c03f4fab4854d24ed0
Arundhathi2282 <arundhathikuricheti@gmail.com>
2021-05-26 12:38:41 +0530

    Add : Calculating 2nd eaxpression

--- arithmaticComputation.sh
+++ arithmaticComputation.sh
@@ -3,4 +3,6 @@ read -p "Enter 1st input : " a
 read -p "Enter 2nd input : " b
 read -p "Enter 3rd input : " c
 w=`awk 'BEGIN{print '$a' + '$b' * '$c' }'`
+x=`awk 'BEGIN{print '$a' * '$b' + '$c' }'`
 echo "First expression output : $w"
+echo "Second expression output : $x"
